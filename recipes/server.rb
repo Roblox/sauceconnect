122a8851d1d09155502722eb368ca9461952a5e5
Julian C. Dunn <jdunn@chef.io>
2015-07-21 23:05:06 -0400

    Since Java is no longer required, don't include_recipe it.

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,6 +3,7 @@
 # Recipe:: server
 #
 # Copyright 2012-2013, SecondMarket Labs, LLC
+# Copyright 2013-2015, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,8 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-include_recipe 'java'
 
 user node['sauceconnect']['server']['user'] do
   comment 'SauceLabs Proxy User'
