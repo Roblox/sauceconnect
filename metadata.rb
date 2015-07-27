7a8cb1ddbbb0ac01620c39c9714a5774bd2c5649
Julian C. Dunn <jdunn@chef.io>
2015-07-27 13:35:07 -0400

    Prepare for 0.2.0 release

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,14 @@
 name             'sauceconnect'
 maintainer       'Chef Software, Inc.'
-maintainer_email 'jdunn@getchef.com'
+maintainer_email 'jdunn@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures SauceLabs Connect Proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.10'
+version          '0.2.0'
+issues_url       'https://github.com/juliandunn/sauceconnect/pulls'
+source_url       'https://github.com/juliandunn/sauceconnect'
 
 %w(fedora redhat centos oracle amazon scientific).each do |p|
   supports p
 end
+
